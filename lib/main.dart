7c860cc7c515c5e84fe2f3935e4ebb277da21a76
fuguoqiang <434543162@qq.com>
2022-04-26 00:54:35 +0800

    Column属性

--- lib/main.dart
+++ lib/main.dart
@@ -12,10 +12,14 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
             child: Column(
+          // mainAxisSize: MainAxisSize.min,
+          // verticalDirection: VerticalDirection.up,
+          // mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             Container(
               height: 100,
-              width: 100,
+
               // margin: EdgeInsets.all(10),
               // margin: EdgeInsets.only(left: 10, right: 10, top: 10, bottom: 10),
               // margin: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
@@ -23,14 +27,18 @@ class MyApp extends StatelessWidget {
               color: Colors.white,
               child: Text('text1'),
             ),
+            SizedBox(
+              height: 10,
+            ),
             Container(
-              width: 100,
               height: 100,
               color: Colors.blue,
               child: Text('text2'),
             ),
+            SizedBox(
+              height: 10,
+            ),
             Container(
-              width: 100,
               height: 100,
               color: Colors.red,
               child: Text('text3'),
