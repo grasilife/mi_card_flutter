ff7a0b598c9848892794d3359167e25c0d168da8
fuguoqiang <434543162@qq.com>
2022-04-26 00:30:24 +0800

    Column

--- lib/main.dart
+++ lib/main.dart
@@ -11,16 +11,20 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.blue,
         body: SafeArea(
-          child: Container(
-            height: 100,
-            width: 100,
-            // margin: EdgeInsets.all(10),
-            margin: EdgeInsets.only(left: 10, right: 10, top: 10, bottom: 10),
-            // margin: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-            color: Colors.white,
-            child: Text('text'),
-          ),
-        ),
+            child: Column(
+          children: <Widget>[
+            Container(
+              height: 100,
+              width: 100,
+              // margin: EdgeInsets.all(10),
+              // margin: EdgeInsets.only(left: 10, right: 10, top: 10, bottom: 10),
+              // margin: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+              margin: EdgeInsets.all(10),
+              color: Colors.white,
+              child: Text('text'),
+            ),
+          ],
+        )),
       ),
     );
   }
