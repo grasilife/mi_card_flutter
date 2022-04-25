ae39acf049ab3f640ec16692fd3182dd33529499
fuguoqiang <434543162@qq.com>
2022-04-26 00:33:28 +0800

    多个Container元素

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.blue,
+        backgroundColor: Colors.teal,
         body: SafeArea(
             child: Column(
           children: <Widget>[
@@ -19,10 +19,22 @@ class MyApp extends StatelessWidget {
               // margin: EdgeInsets.all(10),
               // margin: EdgeInsets.only(left: 10, right: 10, top: 10, bottom: 10),
               // margin: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-              margin: EdgeInsets.all(10),
+              // margin: EdgeInsets.all(10),
               color: Colors.white,
-              child: Text('text'),
+              child: Text('text1'),
             ),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.blue,
+              child: Text('text2'),
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.red,
+              child: Text('text3'),
+            )
           ],
         )),
       ),
