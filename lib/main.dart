5ee5b92b119dc3efe6b3390a62e8cc1866ed847c
fuguoqiang <434543162@qq.com>
2022-04-26 09:57:12 +0800

    CircleAvatar

--- lib/main.dart
+++ lib/main.dart
@@ -11,37 +11,11 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-            child: Row(
-          // mainAxisSize: MainAxisSize.min,
-          // verticalDirection: VerticalDirection.up,
-          // mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
+            child: Column(
           children: <Widget>[
-            Container(
-              height: 100,
-
-              // margin: EdgeInsets.all(10),
-              // margin: EdgeInsets.only(left: 10, right: 10, top: 10, bottom: 10),
-              // margin: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-              // margin: EdgeInsets.all(10),
-              color: Colors.white,
-              child: Text('text1'),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Container(
-              height: 100,
-              color: Colors.blue,
-              child: Text('text2'),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Container(
-              height: 100,
-              color: Colors.red,
-              child: Text('text3'),
+            CircleAvatar(
+              radius: 50,
+              backgroundImage: AssetImage('images/mega-creator.png'),
             )
           ],
         )),
